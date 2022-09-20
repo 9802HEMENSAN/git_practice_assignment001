14f300cc6225a4929e10bf757caea707cc7dd7dd
hemensan mahilange <hmahilange9802@gmail.com>
2022-09-20 21:19:16 +0530

    prime and palindrome functionalities

---
+++ prime.js
@@ -0,0 +1,16 @@
+ // Problem 1: Create a function to check if a number is Prime or Not
+function checkPrime(num){
+  let factor=0;
+ for(let i=1; i<=num; i++){
+  if(num%i===0){
+     factor++;
+   }
+ }
+ if(factor==2){
+   console.log(num,"is prime");
+ }else{
+   console.log(num,"is not a prime");
+ }
+ }
+  
+ checkPrime(13);
