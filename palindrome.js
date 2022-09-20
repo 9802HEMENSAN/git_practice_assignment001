cf3f621e1be3b95cf2bf9c4e364e22582b424b34
Rdev921 <rishabhd921@gmail.com>
2022-09-20 22:54:29 +0530

    rishabhDev commit

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+function checkPal(str){
+    let bag="";
+    for(let i=str.length-1; i>=0; i--){
+            bag+=str[i];
+    }
+    if(bag === str){
+        return true;
+    }
+    return false;
+}
+    
+let answer = checkPal("racecar");
+if(answer == true){
+    console.log("string is palindrome");
+}else{
+    console.log("string is not palindrome");
+
+}
